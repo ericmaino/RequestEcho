278bf06366a8497cec4d636b7e086da99e2c2235
Eric Maino <eric.maino@microsoft.com>
2020-05-04 13:53:18 -0700

    WIP

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,17 @@ class ExpressApp {
     }
 
     public ConfigureAndRun() {
+        this.app.options('/:key', async (req, res) => {
+            logger.Trace("OPTIONS");
+            res.status(401).send();
+        });
+
         this.app.get('/health', async (req, res) => {
             res.status(200).send({ health: "Ok" });
         });
 
         this.app.get('/:key', async (req, res) => {
+            logger.Trace("GET");
             const keyName = req.params.key;
             const result = {};
 
@@ -28,7 +34,29 @@ class ExpressApp {
             });
 
             if (!keyName || !result[keyName]) {
-                res.status(401).send();
+                logger.Trace("log", result);
+
+                if (req.headers["authorization"]) {
+                    logger.Trace("Redirect");
+                    res
+                        .set(
+                            {
+                                'www-authenticate': 'Bearer authorization_uri=https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/authorize?client_id=2891fa56-d262-4d83-9510-96936c128941&response_mode=fragment&response_type=code+id_token&redirect_uri=https%3A%2F%2Frequest-echo.azurewebsites.net%2Fauthorize&scope=openid&nonce=12345'
+                            }
+                        )
+                        .status(401).send();
+                } else {
+                    logger.Trace("Auth");
+                    res
+                        .set(
+                            {
+                                'content-type': 'application/json; odata.metadata=minimal',
+                                'odata-version': '4.0',
+                                'www-authenticate': 'Bearer realm=https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47'
+                            }
+                        )
+                        .status(401).send();
+                }
                 return;
             }
 
